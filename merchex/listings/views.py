7f98fc4bb113cacdbffe8266d0e935219f3e7de1
John <john.t.singer@gmail.com>
2022-02-15 09:29:10 +0100

    Ajout des pages listings et contact-us

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,5 +5,12 @@ from django.http import HttpResponse
 def hello(request):
     return HttpResponse('<h1>Hello Django !</h1>')
 
+def listings(request):
+    return HttpResponse('<h1>Listings</h1> <ul><li>élément 1</li> <li>élément 2</li></ul>')
+
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contactez nous</h1> <p>Formulaire de contact</p>')
+
