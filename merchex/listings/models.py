83f09e4d519aad6ee6339b42013ca514c6baa25f
John <john.t.singer@gmail.com>
2022-03-10 10:28:29 +0100

    Création d'une classe Genre dans le modèle Band

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -3,8 +3,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
         validators=[MinValueValidator(1900), MaxValueValidator(2022)]
