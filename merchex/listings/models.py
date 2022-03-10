f3b00f74de30e005f2ed534bfe6926707cf61461
John <john.t.singer@gmail.com>
2022-03-10 10:14:00 +0100

    Ajout de champs suplémentaire au modèle Band

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
 
 class Listing(models.Model):
     title = models.fields.CharField(max_length=100)
