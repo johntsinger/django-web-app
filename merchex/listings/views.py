48c1242453d9c09e74ce8fd0186f6c9cabec111c
John <john.t.singer@gmail.com>
2022-03-31 12:17:23 +0200

    Ajout d'une instruction if pour gèrer les methode GET et POST dans la vue

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -33,7 +33,11 @@ def about(request):
         "listings/about.html")
 
 def contact(request):
-    form = ContactUsForm()
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+    else:
+        form = ContactUsForm()
+
     return render(request,
         "listings/contact.html",
         {'form': form})
