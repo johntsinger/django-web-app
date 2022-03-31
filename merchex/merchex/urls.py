04f32daad64fbd5eaba3cb7abb984570d1583ffd
John <john.t.singer@gmail.com>
2022-03-31 10:33:01 +0200

    Mise a jour de urls.py pour ajouter le paramètre name au path contact-us

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('listings/<int:listing_id>/', views.listing_detail,
          name='listing-detail'),
     path('about-us/', views.about),
-    path('contact-us/', views.contact),
+    path('contact-us/', views.contact, name='contact'),
 ]
