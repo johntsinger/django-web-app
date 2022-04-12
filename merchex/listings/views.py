8ba4a371448a5680eb2b4a39dbc75650308a64ee
John <john.t.singer@gmail.com>
2022-04-12 10:17:02 +0200

    Ajout de l'url creation de la vue et du gabarit pour la page band/add

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -58,3 +58,7 @@ def contact(request):
 def email_sent(request):
     return render(request,
         "listings/email_sent.html")
+
+def band_create(request):
+    return render(request,
+        "listings/band_create.html")
