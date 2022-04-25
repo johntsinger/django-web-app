d27bb67050e277e63a6a45a4d9a957cb3c399937
John <john.t.singer@gmail.com>
2022-04-25 19:17:11 +0200

    Ajout d'un widget textarea a ContactUsForm, BandForm et ListingForm

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -5,7 +5,7 @@ from listings.models import Band, Listing
 class ContactUsForm(forms.Form):
     name = forms.CharField(required=False)
     email = forms.EmailField()
-    message = forms.CharField(max_length=1000)
+    message = forms.CharField(max_length=1000, widget=forms.Textarea)
 
 
 class BandForm(forms.ModelForm):
@@ -13,9 +13,15 @@ class BandForm(forms.ModelForm):
         model = Band
         # fields = '__all__'
         exclude = ('active', 'official_homepage')
+        widgets = {
+            'biography': forms.Textarea
+        }
 
 
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
         exclude = ('sold',)
+        widgets = {
+            'description': forms.Textarea
+        }
